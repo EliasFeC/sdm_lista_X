3cb0f3d889daaa8d44ef9d6184e22e5ba0a178df
Elias Ferreira Coelho <151461511+EliasFeC@users.noreply.github.com>
2024-11-14 10:02:04 -0300

    Add files via upload

---
+++ ConselhoApi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ConsumerAdviceApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://api.adviceslip.com/advice";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Fazendo um GET na API
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    // Lendo a resposta
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Analisando a resposta
+                    JObject json = JObject.Parse(responseBody);
+                    string advice = json["slip"]["advice"].ToString();
+
+                    Console.WriteLine("\nConselho de Hoje: \n" + advice);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ocorreu um erro: " + e.Message);
+                }
+            }
+        }
+    }
+}
